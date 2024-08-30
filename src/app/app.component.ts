7e74a6b686457b8345c2ed577c9af01b3038eacb
brunascauri <brunascauri@hotmail.com>
2024-08-30 19:01:15 -0300

    perf: content projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { SupportTicketsComponent } from "./dashboard/support-tickets/support-tickets.component";
 import { TrafficStatusComponent } from "./dashboard/traffic-status/traffic-status.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [ServerStatusComponent, SupportTicketsComponent, TrafficStatusComponent, HeaderComponent],
+  imports: [ServerStatusComponent, SupportTicketsComponent, TrafficStatusComponent, HeaderComponent, DashboardItemComponent],
 })
 export class AppComponent {
 
