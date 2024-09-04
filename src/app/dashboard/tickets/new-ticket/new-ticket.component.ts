9d6880de712278f624402cfbf7eecbb30e9d0b80
brunascauri <brunascauri@hotmail.com>
2024-09-04 19:09:22 -0300

    feat: ViewChild

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from "../../../shared/button/button.component";
@@ -7,12 +7,17 @@ import { ControlComponent } from "../../../shared/control/control.component";
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(titleInput: HTMLInputElement) {
-    console.log(titleInput)
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  // private form = viewChild<ElementRef<HTMLFormElement>>('form') //can also be set like this
+
+  onSubmit(titleInput: string, requestInput: string) {
+    // console.log(titleInput, requestInput)
+    this.form?.nativeElement.reset()
   }
 }
